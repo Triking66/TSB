8283ebcc7a2bb1fb641e4f25c9114d96d595cc57
DESKTOP-2U81M9K\Dell <sujay.tripathy@gmail.com>
2019-03-29 13:06:57 -0600

    blood
    added bloodsplatter

---
+++ TSB/Assets/decalInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class decalInfo : MonoBehaviour
+{
+    [SerializeField]
+    private float bloodDisappear;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, bloodDisappear);
+    }
+}
